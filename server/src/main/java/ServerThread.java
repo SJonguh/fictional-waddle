866a11c0d505b6046c88cf1d9e439eeb308fba17
Astraic <61140790+StephandeJongh@users.noreply.github.com>
2022-03-15 01:51:33 +0100

    Updated Handler + added Progressbar
    Update FileHandler responsibilities and added ProgressBar to indicate progress on file operations.

--- server/src/main/java/ServerThread.java
+++ server/src/main/java/ServerThread.java
@@ -1,4 +1,9 @@
-import java.io.*;
+import utilities.FileHandler;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ServerThread extends Thread {
@@ -13,18 +18,13 @@ public class ServerThread extends Thread {
     public void run() {
         try (
             socket;
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            DataInputStream in = new DataInputStream(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
         ) {
-            if (in.available() > 0) {
-                var firstLine = in.readUTF();
-                System.out.println(firstLine);
-
-                FileHandler fh = new FileHandler("output/video.mp4");
-                fh.write(in, 47_224_582L);
-
-                var lastLine = in.readUTF();
-                System.out.println(lastLine);
+            if (in.ready()) {
+                File file = new File("/files/output/video3.mp4");
+                FileHandler fileHandler = new FileHandler(file);
+                fileHandler.writeFile(socket.getInputStream(), 5_198_882_692L);
             }
         } catch (Exception e) {
             e.printStackTrace();
