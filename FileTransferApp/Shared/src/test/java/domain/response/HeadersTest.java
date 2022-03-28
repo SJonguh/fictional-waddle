291f3aeaa1819e08dd2006969688e0676f3f0700
SavingPrivateHenk <61140909+SavingPrivateHenk@users.noreply.github.com>
2022-03-28 14:17:20 +0200

    UnitTest HeadersTest created

---
+++ FileTransferApp/Shared/src/test/java/domain/response/HeadersTest.java
@@ -0,0 +1,27 @@
+package domain.response;
+
+import domain.Headers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HeadersTest {
+    private Headers headers;
+
+    @BeforeEach
+    void setUp() {
+        headers = new Headers();
+    }
+
+    @Test
+    void setTest() {
+        // Prepare
+        String key = "";
+        String value = "";
+        // Act
+        headers.set(key, value);
+        // Assert
+        assertEquals(value, headers.get(key));
+    }
+}
