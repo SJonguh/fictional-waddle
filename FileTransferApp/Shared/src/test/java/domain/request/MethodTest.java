773083b23344a3632e3002cb9c9c7c3ad669ad39
SavingPrivateHenk <61140909+SavingPrivateHenk@users.noreply.github.com>
2022-03-28 11:44:46 +0200

    First Unit test!!!

---
+++ FileTransferApp/Shared/src/test/java/domain/request/MethodTest.java
@@ -0,0 +1,47 @@
+package domain.request;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MethodTest {
+    private Method method;
+
+    @BeforeEach
+    void setUp() {
+        method = new Method();
+    }
+
+    @Test
+    void testWithRequestType() {
+        // Prepare
+        RequestType requestType = RequestType.FETCH;
+
+        // Act
+        method.withRequestType(requestType);
+        // Assert
+        assertEquals(RequestType.FETCH, method.getRequestType());
+    }
+
+    @Test
+    void testWithPath() {
+        // Prepare
+        String path = "C:/Sample/Data";
+        // Act
+        method.withPath(path);
+        // Assert
+        assertEquals(path, method.getPath());
+    }
+
+    @Test
+    void testWithVersion() {
+        // Prepare
+        String version = "Version 1.0";
+        // Act
+        method.withVersion(version);
+        // Assert
+        assertEquals(version, method.getVersion());
+    }
+
+}
